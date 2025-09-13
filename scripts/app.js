e804d8f44173c8c693e812b5b4a5866a4e1cf2cd
q2t-project <q2t-project@users.noreply.github.com>
2025-09-13 12:23:19 +0000

    deploy: de7841f81281a302589940d96a2be4329fa50c25

--- scripts/app.js
+++ scripts/app.js
@@ -40,8 +40,8 @@ const controls = new OrbitControls(camera, renderer.domElement);
 controls.enableDamping = true;
 controls.dampingFactor = 0.1;
 controls.enableZoom = true;
-controls.zoomSpeed = 0.001;       // ズーム感度（1.0基準、0.5なら緩やか）
-controls.minDistance = 0.001;  // 近づきすぎを防止
+controls.zoomSpeed = 0.01;       // ズーム感度（1.0基準、0.5なら緩やか）
+controls.minDistance = 0.01;  // 近づきすぎを防止
 controls.maxDistance = 100;   // ズームアウトしすぎ防止
 
 // 重要: ホイールを連続ズームにする
